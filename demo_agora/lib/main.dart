abb36f1d702d247502188201c0e7d2bfb2dd26af
pushkar-appdid <130537876+pushkar-appdid@users.noreply.github.com>
2023-08-01 16:39:01 +0530

    init commit

--- demo_agora/lib/main.dart
+++ demo_agora/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:demo_agora/firebase_options.dart';
 import 'package:demo_agora/screens/homepage.dart';
 import 'package:demo_agora/screens/loginscreen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
@@ -39,9 +40,27 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: HomePage(),
+      home: FutureBuilder(
+        future: FirebaseAuth.instance.authStateChanges().first,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            // If the connection state is waiting, show a loading screen
+            return const Scaffold(
+              body: Center(child: CircularProgressIndicator()),
+            );
+          } else {
+            // Check if the user is authenticated or not
+            final isUserLoggedIn = snapshot.hasData;
+
+            // Show the appropriate screen based on user authentication status
+            if (isUserLoggedIn) {
+              return const HomePage();
+            } else {
+              return const LoginScreen();
+            }
+          }
+        },
+      ),
     );
   }
 }
-
-
